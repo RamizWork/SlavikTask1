a56ec4ac796739ca98c42258697f99b1616af67a
RamizD <RamizWorking@gmail.com>
2022-05-23 09:08:06 +0300

    Add angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {TodoInterface} from "../shared/interfaces/todo.interface";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'salavikTaskTest1';
+export class AppComponent implements OnInit {
+
+  todoData: TodoInterface[] = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+      this.http.get<TodoInterface[]>('https://jsonplaceholder.typicode.com/todos?_limit=20')
+      .subscribe(response => {
+        this.todoData = response;
+        console.log(this.todoData);
+      })
+  }
 }
